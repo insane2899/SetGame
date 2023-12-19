87d441755aef8e5fdfe441442cdbef46b7df86a8
Soumik Sen <imrimosen@gmail.com>
2023-12-19 12:54:44 +0530

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Soumik Sen on 19/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Soumik Sen on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
